61cdd38ee528b52d803f23a06dcd14d6c2154253
Zeph Lafassett <zeph@lafassett.com>
2014-09-30 17:28:08 -0700

    moving template files around

--- test/render-file-replace.js
+++ test/render-file-replace.js
@@ -4,13 +4,13 @@ var assert = require('assert'),
 
 describe('Render File tests', function () {
 	it('file replace on simple string', function (done) {
-		var result = tinytim.renderFile("test/string.tim", {place: "world"});
+		var result = tinytim.renderFile("test/files/string.tim", {place: "world"});
 		assert.equal(result, "Hello world");
 		done();
 	});
 
 	it('file path replace on simple string', function (done) {
-		var template = "test/string2.tim",
+		var template = "test/files/string2.tim",
 			data = {
 				place: "Brighton",
 				person: {
@@ -24,7 +24,7 @@ describe('Render File tests', function () {
 	});
 
 	it('file replace on html string', function (done) {
-		var template = "test/html.tim",
+		var template = "test/files/html.tim",
 			data = {
 				title: "Dharmafly",
 				url: "http://dharmafly.com"
@@ -32,21 +32,6 @@ describe('Render File tests', function () {
 
 		var result = tinytim.renderFile(template, data);
 		assert.equal(result, "<p><a href='http://dharmafly.com'>Dharmafly</a></p>");
-		done();
-	});
-
-	it('inline replace on nested object', function (done) {
-		var ul = "<ul>{{list}}</ul>",
-			li = "<li>{{contents}}</li>",
-			myList = "",
-			i;
-
-		for (i = 100; i < 103; i++) {
-			myList += tim(li, {contents: i});
-		}
-		var result = tim(ul, {list: myList});
-		assert.equal(result, "<ul><li>100</li><li>101</li><li>102</li></ul>");
-
 		done();
 	});
 
